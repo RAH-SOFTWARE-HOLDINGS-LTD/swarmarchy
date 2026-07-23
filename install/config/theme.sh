9ff7896fb55aa8588c4219f5ee4651c318fa6308
Ralph A <ralphie02@live.com>
2026-07-23 00:56:15 -0700

    don't use yaru icon and make localsend & obsidian optional

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -1,6 +1,4 @@
-# Set links for Nautilus action icons
-sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
-sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
+# (Yaru icon theme dropped on aarch64 — Nautilus uses Adwaita's action icons directly, no patch needed.)
 
 # Setup user theme folder
 mkdir -p ~/.config/swarmarchy/themes
--- install/first-run/gnome-theme.sh
+++ install/first-run/gnome-theme.sh
@@ -1,5 +1,4 @@
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
-
-sudo gtk-update-icon-cache /usr/share/icons/Yaru
+# Adwaita icon theme (from gnome-themes-extra); Yaru was dropped on aarch64 (no repo source).
+gsettings set org.gnome.desktop.interface icon-theme "Adwaita"
