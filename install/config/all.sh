990df5237b69f465b5d5de6a6a6f9f0e84773335
Ralph A <ralphie02@live.com>
2026-08-04 02:13:34 -0700

    I don't care about supporting extra hw right now..

--- install/config/all.sh
+++ install/config/all.sh
@@ -36,29 +36,6 @@ run_logged $SWARMARCHY_INSTALL/config/hardware/ignore-power-button.sh
 run_logged $SWARMARCHY_INSTALL/config/hardware/nvidia.sh
 run_logged $SWARMARCHY_INSTALL/config/hardware/vulkan.sh
 
-run_logged $SWARMARCHY_INSTALL/config/hardware/intel/video-acceleration.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/intel/lpmd.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/intel/thermald.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/intel/ipu7-camera.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/intel/ptl-kernel.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/intel/fred.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/intel/fix-wifi7-eht.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/intel/sof-firmware.sh
-
-run_logged $SWARMARCHY_INSTALL/config/hardware/asus/fix-asus-ptl-display-backlight.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/asus/fix-asus-ptl-b9406-display.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/asus/fix-asus-ptl-b9406-touchpad.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/asus/fix-audio-mixer.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/asus/fix-mic.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/asus/fix-z13-touchpad.sh
-
-run_logged $SWARMARCHY_INSTALL/config/hardware/framework/fix-f13-amd-audio-input.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/framework/qmk-hid.sh
-
-run_logged $SWARMARCHY_INSTALL/config/hardware/apple/fix-spi-keyboard.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/apple/fix-suspend-nvme.sh
-run_logged $SWARMARCHY_INSTALL/config/hardware/apple/fix-t2.sh
-
 run_logged $SWARMARCHY_INSTALL/config/hardware/lenovo/fix-yoga-pro7-bass-speakers.sh
 
 run_logged $SWARMARCHY_INSTALL/config/hardware/fix-bcm43xx.sh
